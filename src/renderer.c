a49f617db9be9505f59481792ad5958cdc4e4b49
ttchef <ruro0.edit@gmail.com>
2025-11-14 09:39:09 +0100

    split audio into colored blocks

--- src/renderer.c
+++ src/renderer.c
@@ -19,6 +19,27 @@ void renderGui(Context* context) {
     int32_t maxHeight = 500;
     DrawRectangle(startX, currentY, elementWidth, maxHeight, BLACK);
     int32_t volume = fmin(fmax(atomic_load(&context->data.volL), atomic_load(&context->data.volR)), maxHeight);
-    DrawRectangle(startX + elementWidth / 4, currentY + maxHeight - paddingY - volume, elementWidth / 2, volume, RED);
+
+    int32_t barX = startX + elementWidth / 4;
+    int32_t barW = elementWidth / 2;
+    int32_t barYbottom = currentY + maxHeight - paddingY;
+
+    for (int32_t i = 0; i < volume; i++) {
+        float t = i / (float)maxHeight;
+        
+        Color color;
+        if (t < 0.5f) {
+            float k = t / 0.5f;
+            color = (Color){(uint8_t)(k * 255), 255, 0, 255};
+        }
+        else {
+            float k = (t - 0.5f) / 0.5f;
+            color = (Color){255, (uint8_t)((1.0f - k) * 255), 0, 255};
+        }
+        DrawRectangle(barX, barYbottom - i, barW, 1, color);
+    }
+
+    //DrawRectangle(startX + elementWidth / 4, currentY + maxHeight - paddingY - volume, elementWidth / 2, volume, RED);
+    currentY += maxHeight + paddingY;
 }          
 
